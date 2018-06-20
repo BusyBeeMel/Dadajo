b7d361ca9089f3e29ec120ba7a0e483148eb3d12
LeeSeonggyeong <kij0621@naver.com>
2018-06-20 17:05:54 +0900

    v12

--- app/src/main/java/com/example/student/dadajo/SecondFragment.java
+++ app/src/main/java/com/example/student/dadajo/SecondFragment.java
@@ -36,8 +36,8 @@ public class SecondFragment extends Fragment {
     private String title;
     private int page;
     MultiLineChart multiLineChart;
-    List<ChartData> InDust;
-    List<ChartData> OutDust;
+    //List<ChartData> InDust;
+    //List<ChartData> OutDust;
     private Handler mHandler;
     private Thread mThread;
 
@@ -59,9 +59,10 @@ public class SecondFragment extends Fragment {
         page = getArguments().getInt("someInt", 1);
         title = getArguments().getString("someTitle");
 
-        InDust = new ArrayList<>();
-        OutDust = new ArrayList<>();
-        getDustValue();
+
+        //InDust = new ArrayList<>();
+        //OutDust = new ArrayList<>();
+
     }
 
     // Inflate the view for the fragment based on layout XML
@@ -71,6 +72,9 @@ public class SecondFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
 
         multiLineChart = (MultiLineChart) view.findViewById(R.id.chart);
+        multiLineChart.setScaleY(1);
+        getDustValue();
+
         List<String> h_lables = new ArrayList<>();
         h_lables.add("1");
         h_lables.add("2");
@@ -87,61 +91,12 @@ public class SecondFragment extends Fragment {
 
         multiLineChart.setHorizontal_label(h_lables);
 
-
-        showChart();
-
-
-        /*multiLineChart.setScaleY(1);
-        List<ChartData> InDust = new ArrayList<>();
-
-        InDust.add(new ChartData(30f, 0f)); //values.add(new ChartData(y,x));<br />
-        InDust.add(new ChartData(20f, 1f));
-        InDust.add(new ChartData(20f, 2f));
-        InDust.add(new ChartData(5f, 3f));
-        InDust.add(new ChartData(9f, 4f));
-        InDust.add(new ChartData(8f, 5f));
-        InDust.add(new ChartData(6f, 6f));
-        InDust.add(new ChartData(7f, 7f));
-        InDust.add(new ChartData(10f, 8f));
-        InDust.add(new ChartData(11f,9f));
-        InDust.add(new ChartData(13f,10f));
-
-        List<ChartData> OutDust = new ArrayList<>();
-        OutDust.add(new ChartData(4f, 0f));
-        OutDust.add(new ChartData(5f, 1f));
-        OutDust.add(new ChartData(6f, 2f));
-        OutDust.add(new ChartData(7f, 3f));
-        OutDust.add(new ChartData(7f, 4f));
-        OutDust.add(new ChartData(9f, 5f));
-        OutDust.add(new ChartData(5f, 6f));
-        OutDust.add(new ChartData(3f, 7f));
-        OutDust.add(new ChartData(5f, 8f));
-        OutDust.add(new ChartData(15f,9f));
-        OutDust.add(new ChartData(15f,10f));
-
-        List<ChartData> chart = new ArrayList<>();
-        chart.add(new ChartData(InDust));
-        chart.add(new ChartData(OutDust));
-
-        multiLineChart.setLeft(1);
-        multiLineChart.setRight(50);
-        //    mScaleFactor = jononoj  Math.max(.1f, Math.min(mScaleFactor, 10.0f));
-
-
-        //define the lines
-        List<String> legends = new ArrayList<>();
-        legends.add("In");
-        legends.add("Out");
-        multiLineChart.setLegends(legends);
-        //string value of the x axis
-        multiLineChart.setCircleSize(8f);
-        multiLineChart.setData(chart);
-        multiLineChart.setGesture(true);*/
         return view;
     }
 
     //한 시간마다 미세먼지 데이터 받아오는 메서드
     public void getDustValue() {
+
 
         Timer t = new Timer();
 
@@ -150,18 +105,28 @@ public class SecondFragment extends Fragment {
                 {
                     public void run()
                     {
+                        Queue inDustValue=null;
+                        Queue outDustValue=null;
                         try {
                             Response<Queue> inGraph = SensorApi.service.getChartIn().execute(); // 현재 스레드에서 네트워크 작업 요청.
                             Response<Queue> outGraph = SensorApi.service.getChartOut().execute(); //Out 차트 받아오기
 
                             if (inGraph.code() == 200) {
-                                Queue result = inGraph.body();
-                                if (result == null) {
+                                inDustValue = inGraph.body();
+                                if (inDustValue == null) {
                                     //System.out.println("window 가져오기 실패");
                                     Log.d("결과", "Inchart 가져오기 실패");
                                 } else {
                                     // System.out.println("window 가져오기 성공");
-                                    Log.d("결과", "Inchart 가져오기 성공 " + result);
+                                    Log.d("결과", "Inchart 가져오기 성공 " + inDustValue);
+
+                                   /* for(Integer i=0; i<12; i++){
+                                        Float y=Float.parseFloat(result.poll().toString()+"f");
+                                        Float x=Float.parseFloat(i.toString()+"f");
+                                        InDust.add(new ChartData(y,x));
+                                        Log.d("차트값", "Inchart Y " + y);
+                                        //Log.d("차트값", "Inchart X " + x);
+                                    }*/
                                 }
                             } else {
                                 // System.out.println("에러 코드: "+res.code());
@@ -169,14 +134,24 @@ public class SecondFragment extends Fragment {
                             }
 
 
+
+
                             if (outGraph.code() == 200) {
-                                Queue result2 = outGraph.body();
-                                if (result2 == null) {
+                                outDustValue = outGraph.body();
+
+
+
+                                if (outDustValue == null) {
                                     //System.out.println("window 가져오기 실패");
                                     Log.d("결과", "Outchart 가져오기 실패");
                                 } else {
                                     // System.out.println("window 가져오기 성공");
-                                    Log.d("결과", "Outchart 가져오기 성공 " + result2);
+                                    Log.d("결과", "Outchart 가져오기 성공 " + outDustValue);
+                                    /*for(Integer i=0; i<12; i++){
+                                        Float y=Float.parseFloat(result2.poll()+"f");
+                                        Float x=Float.parseFloat(i.toString()+"f");
+                                        OutDust.add(new ChartData(y,x));
+                                    }*/
                                 }
                             } else {
                                 // System.out.println("에러 코드: "+res.code());
@@ -185,97 +160,38 @@ public class SecondFragment extends Fragment {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
+
+                        showChart(inDustValue,outDustValue);
+
                     }
                 },
                 0,
-                5000);
+                3600000);
+
+
     };
 
 
-
-
-
-
-/*        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {*/
-        /*Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    Looper.prepare();
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable(){
-
-                        @Override
-                        public void run(){
-                            try{
-                                Response<Queue> res = SensorApi.service.getChartIn().execute(); // 현재 스레드에서 네트워크 작업 요청.
-                                if (res.code() == 200) {
-                                    Queue result = res.body();
-                                    if (result == null) {
-                                        //System.out.println("window 가져오기 실패");
-                                        Log.d("결과", "chart 가져오기 실패");
-                                    } else {
-                                        // System.out.println("window 가져오기 성공");
-                                        Log.d("결과", "chart 가져오기 성공 " + result);
-                                    }
-                                } else {
-                                    // System.out.println("에러 코드: "+res.code());
-                                    Log.d("결과", "에러 코드: " + res.code());
-                                }
-                            }catch(IOException e){
-                                    e.printStackTrace();
-                            }
-                        }
-                    }, 5000);
-
-
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-        });*/
-
-
-             /*   new Thread() {
-                    public void run() {
-                        try {
-                            Response<Queue> res = SensorApi.service.getChartIn().execute(); // 현재 스레드에서 네트워크 작업 요청.
-                            if(res.code()==200) {
-                                Queue result = res.body();
-                                if(result == null) {
-                                    //System.out.println("window 가져오기 실패");
-                                    Log.d("결과","chart 가져오기 실패");
-                                }else {
-                                    // System.out.println("window 가져오기 성공");
-                                    Log.d("결과","chart 가져오기 성공 " + result);
-                                }
-                            }else {
-                                // System.out.println("에러 코드: "+res.code());
-                                Log.d("결과","에러 코드: "+res.code());
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }.start();*/
- /*           }
-        },5000);
-        }
-*/
-
-
     //한 시간마다 차트 뿌려주는 메서드
-    public void showChart(){
-        multiLineChart.setScaleY(1);
+    public void showChart(Queue inDustValue, Queue outDustValue){
+        List<ChartData> InDust = new ArrayList<>();
+        List<ChartData> OutDust = new ArrayList<>();
 
-        InDust.add(new ChartData(30f, 0f)); //values.add(new ChartData(y,x));<br />
+        for(Integer i=0; i<12; i++){
+            Float y=Float.parseFloat(inDustValue.poll().toString()+"f");
+            Float x=Float.parseFloat(i.toString()+"f");
+            InDust.add(new ChartData(y,x));
+            Log.d("차트값", "Inchart Y " + y);
+            //Log.d("차트값", "Inchart X " + x);
+        }
+
+        for(Integer i=0; i<12; i++){
+            Float y=Float.parseFloat(outDustValue.poll()+"f");
+            Float x=Float.parseFloat(i.toString()+"f");
+            OutDust.add(new ChartData(y,x));
+        }
+
+        /*InDust.add(new ChartData(30f, 0f)); //values.add(new ChartData(y,x));<br />
         InDust.add(new ChartData(20f, 1f));
         InDust.add(new ChartData(20f, 2f));
         InDust.add(new ChartData(5f, 3f));
@@ -297,14 +213,13 @@ public class SecondFragment extends Fragment {
         OutDust.add(new ChartData(3f, 7f));
         OutDust.add(new ChartData(5f, 8f));
         OutDust.add(new ChartData(15f,9f));
-        OutDust.add(new ChartData(15f,10f));
+        OutDust.add(new ChartData(15f,10f));*/
 
         List<ChartData> chart = new ArrayList<>();
         chart.add(new ChartData(InDust));
         chart.add(new ChartData(OutDust));
 
-        multiLineChart.setLeft(1);
-        multiLineChart.setRight(50);
+
         //    mScaleFactor = jononoj  Math.max(.1f, Math.min(mScaleFactor, 10.0f));
 
 
